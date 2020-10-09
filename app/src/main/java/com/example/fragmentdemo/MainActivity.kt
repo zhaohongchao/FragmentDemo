d03f9a83b2e338142b26ad3efe60557130c44c4f
zhaohongchao <1586722114@qq.com>
2020-10-09 11:35:54 +0800

    添加Fragment 列表

--- app/src/main/java/com/example/fragmentdemo/MainActivity.kt
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.fragmentdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,13 +16,14 @@ class MainActivity : FragmentActivity() {
     private var f3 = Fragment3()
     private var f4 = Fragment4()
 
-
+    private var listf = arrayListOf<Fragment>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tab1.setupWithViewPager(vp1)
 
+        init()
 
 
 
@@ -31,5 +33,12 @@ class MainActivity : FragmentActivity() {
 
 
 
+    }
+
+    private fun init() {
+        listf.add(f1)
+        listf.add(f2)
+        listf.add(f3)
+        listf.add(f4)
     }
 }
